88697396d3e154dff6b06cefa5f95ee4e4027cb5
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-21 13:03:45 +0000

    add new authRoutes

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 app.use(cors('*'));
 app.use(express.json())
 
-const userRoutes = require('./routes/users')
-const habitRoutes = require('./routes/habits')
+const authRoutes = require('./routes/auths');
+const userRoutes = require('./routes/user')
+const habitRoutes = require('./routes/habit')
 
-app.use('/', userRoutes)
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 app.use('/habits', habitRoutes)
 
 module.exports = app
