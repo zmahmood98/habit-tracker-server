4b164ad6f198bfed5780fa112668b4ce24b5b2ac
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-21 12:49:32 +0000

    fix route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { verifyToken } = require('../middleware/auth')
 
 const userController = require('../controllers/user')
 
-router.get('/', userController.index)
+router.get('/users', userController.index)
 router.get('/users/:name', verifyToken, userController.findByname)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
