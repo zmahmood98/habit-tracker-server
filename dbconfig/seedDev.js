1eb28ba9e8e0afc51164f4c0af6bc061513d579c
Dafe-akaka <dafe.akaka123@gmail.com>
2022-02-21 17:28:55 +0000

    clean up

--- dbconfig/seedDev.js
+++
@@ -1,6 +0,0 @@
-const db = require('./init.js');
-const fs = require('fs');
-
-const seeds = fs.readFileSync(__dirname + '/db.sql').toString();
-
-db.query(seeds, () => console.log('Dev database seeded'));
