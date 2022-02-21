aba413cc06f8b95a2c55d04645f13a3dd30bccd6
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-21 15:14:44 +0000

    add docker files

--- index.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server');
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Express just departed from port ${port}!`))
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken } = require('../middleware/auth')
-
-const userController = require('../controllers/user')
-
-router.get('/', userController.index)
-router.get('/:name', verifyToken, userController.findByname)
-
